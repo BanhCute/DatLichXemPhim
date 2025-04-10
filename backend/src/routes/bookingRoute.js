f9ffe98026c16b386ecb8342b5ed217cf65fd929
NguyenTanQuoc <ksgamer951@gmail.com>
2025-04-10 21:08:10 +0700

    fix getall booking

--- backend/src/routes/bookingRoute.js
+++ backend/src/routes/bookingRoute.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var router = express.Router();
 let bookingController = require("../controllers/bookingController");
-let { CheckAuth } = require("../utils/check_auth");
+let { CheckAuth, CheckRole } = require("../utils/check_auth");
 let { CreateSuccessRes } = require("../utils/responseHandler");
 require("dotenv").config();
 
-router.get("/", async (req, res, next) => {
+router.get("/", [CheckAuth, CheckRole], async (req, res, next) => {
   try {
     let bookings = await bookingController.GetAll();
     CreateSuccessRes(res, bookings, 200);
